1d80b1dba942236f9ab12dbb4c5cadb562532676
alex-grigoras <alex.grigoras@gmail.com>
2016-05-26 14:41:36 +0300

    Update Program.cs

--- 12-Byte-at-a-time-ECB-decryption-Simple/Program.cs
+++ 12-Byte-at-a-time-ECB-decryption-Simple/Program.cs
@@ -19,8 +19,6 @@ namespace sandbox2
                 Convert.FromBase64String(
                     "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK");
 
-            var encrSecret = RandomKeyAESECB.EncryptAESECB(secret);
-
             var i = 0;
             while (i*16 < secret.Length)
             {
